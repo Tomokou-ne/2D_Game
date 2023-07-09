5eb8634a60d4f40387777467e4a65c8983a3ce84
Tomokou <bychckova.margo@yandex.ru>
2023-07-09 23:44:04 +0300

    Done introduction. Pre-final game panel

--- 2D_Game/src/main/GamePanel.java
+++ 2D_Game/src/main/GamePanel.java
@@ -1,5 +1,42 @@
 package main;
 
-public class GamePanel {
+import javax.swing.JPanel;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel implements Runnable{
+	//to have all JPanel's functions
+	
+	//SCREEN SETTINGS
+	final int originalTilesSize = 16; //16x16 pixels
+	final int scale = 3; //scale window resolution to see lil' character
+	
+	final int tileSize = originalTilesSize * scale; //to increase visible tiles == 48x48 pixels
+	final int maxScreenColumn = 16; //width
+	final int maxScreenLines = 12; //height
+	final int screenWidth = maxScreenColumn * tileSize; //768 pixels
+	final int screenHeight = maxScreenLines * tileSize; //576 pixels
+	
+	Thread gameThread; //keeps game running till stop command 
+	
+	public GamePanel() {
+		
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+	}
+
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- 2D_Game/src/main/Main.java
+++ 2D_Game/src/main/Main.java
@@ -1,5 +1,23 @@
 package main;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame(); 
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //set closing game operation
+		window.setResizable(false); //set resizable window to false, for close preventation
+		window.setTitle("2D Game");//set Title of the project. game-name
+		
+		final GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		
+		window.pack();		
+		
+		window.setLocationRelativeTo(null); //set size of the game-screen. null = fullscreen; window = windowed
+		window.setVisible(true); //to see the game-window
+	
+	}
+	
 }
